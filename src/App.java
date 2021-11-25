807c3cc7eeab2a40fe9898939f5578ce329abe32
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-11-25 16:34:14 +0000

    Initial working

--- src/App.java
+++ src/App.java
@@ -1,30 +1,49 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) {
-        // reads text from text file at given URL.
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\steve\\OneDrive - MMU\\Desktop" +
-                "\\testTestTest" +
-                ".txt"))) {
-            StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        String html1 = "<!DOCTYPE html><html><body><p>";
+        String html2 = "<p/></body></html>";
+        String everything = "";
+
+// Load and read text file
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\steve\\OneDrive\\Desktop" +
+                "\\james-joyce-the-dead-paragraph.txt"))) {
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
             String line = br.readLine();
+
+            System.out.println("1 " + html1);
+            System.out.println("2 " + html2);
 
 
             while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
+                sb1.append(line);
+                sb1.append(System.lineSeparator());
                 line = br.readLine();
 
-
             }
-            String everything = sb.toString();
-            System.out.println(everything);
+             everything = sb1.toString();
+
+
+            System.out.println("3 " + everything);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+        // Write to file. Turn into method and pass everything as an agrument
+
+        FileWriter myWriter = new FileWriter("james-joyce-the-dead-paragraph.html");
+        myWriter.write(html1 + everything + html2);
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
+
     }
 }
+
+
 
